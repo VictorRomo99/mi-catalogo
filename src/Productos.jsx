03678f5cf16da75557ceef863e5215daa81903ad
VictorRomo99 <bichitov06@gmail.com>
2025-03-14 00:50:00 -0500

    Corrigiendo rutas de imágenes

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -4,21 +4,21 @@ const productos = [
     nombre: "Memoria RAM 16GB",
     precio: "$80",
     imagen: "/img/ram.jpg.webp", // Ruta directa a public/
-    enlace: "https://wa.me/1234567890",
+    enlace: "https://wa.me/934032635",
   },
   {
     id: 2,
     nombre: "Tarjeta de Video RTX 3060",
     precio: "$400",
     imagen: "/img/grafica.jpg",
-    enlace: "https://wa.me/1234567890",
+    enlace: "https://wa.me/934032635",
   },
   {
     id: 3,
     nombre: "Procesador Ryzen 5 5600X",
     precio: "$200",
     imagen: "/img/ryzen.jpg",
-    enlace: "https://wa.me/1234567890",
+    enlace: "https://wa.me/934032635",
   },
 ];
 
